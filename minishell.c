3f5d5fab56be9f634573105cd8b8128ef28757bf
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2023-11-24 00:23:49 +0100

    refactor: Refactor `minishell.c`

--- minishell.c
+++ minishell.c
@@ -62,17 +62,14 @@
 void store(int *stdinfd, int *stdoutfd, int *stderrfd);
 void redirect(tline *line);
 void auxiliarRedirect(char *filename, const char *MODE, const int STD_FILENO);
-void executeCommand(const tline *line, int number);
+void run(const tline *line, int number);
 void restore(const int stdinfd, const int stdoutfd, const int stderrfd);
+void executeCommands(tline *line);
 
 int main(void)
 {
     char buffer[MAXIMUM_LINE_LENGTH];
     tline *line;
-    int stdinfd, stdoutfd, stderrfd;
-    pid_t pid;
-    int p[PIPE], p2[PIPE];
-    int command;
 
     printf(PROMPT);
     while (fgets(buffer, MAXIMUM_LINE_LENGTH, stdin))
@@ -84,105 +81,7 @@ int main(void)
             continue;
         }
 
-        store(&stdinfd, &stdoutfd, &stderrfd);
-
-        pipe(p);
-
-        pid = fork();
-
-        if (pid == FORK_CHILD)
-        {
-            redirect(line);
-
-            close(p[PIPE_READ]);
-
-            if (line->ncommands > 1)
-            {
-                dup2(p[PIPE_WRITE], STDOUT_FILENO);
-            }
-            close(p[PIPE_WRITE]);
-
-            executeCommand(line, 0);
-        }
-        else
-        {
-            close(p[PIPE_WRITE]);
-
-            wait(NULL);
-
-            restore(stdinfd, stdoutfd, stderrfd);
-
-            for (command = 1; command < line->ncommands; command++)
-            {
-                if (command % 2 == 0)
-                {
-                    pipe(p);
-                }
-                else
-                {
-                    pipe(p2);
-                }
-
-                pid = fork();
-
-                if (pid == FORK_CHILD)
-                {
-                    redirect(line);
-
-                    if (command % 2 == 0)
-                    {
-                        dup2(p2[PIPE_READ], STDIN_FILENO);
-                        if (command != line->ncommands - 1)
-                        {
-                            dup2(p[PIPE_WRITE], STDOUT_FILENO);
-                        }
-                    }
-                    else
-                    {
-                        dup2(p[PIPE_READ], STDIN_FILENO);
-                        if (command != line->ncommands - 1)
-                        {
-                            dup2(p2[PIPE_WRITE], STDOUT_FILENO);
-                        }
-                    }
-                    close(p[PIPE_READ]);
-                    close(p[PIPE_WRITE]);
-                    close(p2[PIPE_READ]);
-                    close(p2[PIPE_WRITE]);
-
-                    executeCommand(line, 1);
-                }
-                else
-                {
-                    if (command % 2 == 0)
-                    {
-                        dup2(STDIN_FILENO, p[PIPE_WRITE]);
-                        close(p[PIPE_WRITE]);
-                        close(p2[PIPE_READ]);
-                        if (command == line->ncommands - 1)
-                        {
-                            close(p[PIPE_READ]);
-                            close(p2[PIPE_WRITE]);
-                        }
-                    }
-                    else
-                    {
-                        dup2(STDIN_FILENO, p2[PIPE_WRITE]);
-                        close(p2[PIPE_WRITE]);
-                        close(p[PIPE_READ]);
-                        if (command == line->ncommands - 1)
-                        {
-                            close(p2[PIPE_READ]);
-                            close(p[PIPE_WRITE]);
-                        }
-                    }
-
-                    wait(NULL);
-                }
-            }
-
-            restore(stdinfd, stdoutfd, stderrfd);
-        }
+        executeCommands(line);
 
         printf(PROMPT);
     }
@@ -257,7 +156,6 @@ void auxiliarRedirect(char *filename, const char *MODE, const int STD_FILENO)
     if (file == NULL)
     {
         fprintf(stderr, "%s: Error. %s\n", filename, strerror(errno));
-        exit(EXIT_FAILURE);
     }
 
     fd = fileno(file);
@@ -269,17 +167,15 @@ void auxiliarRedirect(char *filename, const char *MODE, const int STD_FILENO)
 }
 
 /**
- * Execute a command specified by the given command line structure.
+ * Run a command specified by the given command line structure.
  *
  * @param line A pointer to a `tline` structure representing the command line.
- * @param number The index of the command to be executed within the command
- * line.
+ * @param number The index of the command to be ran within the command line.
  *
  * If the command execution fails, an error message is printed to `stderr`
- * indicating that the command was not found, and the program exits with a
- * failure status.
+ * indicating that was not found, and the program exits with a failure status.
  */
-void executeCommand(const tline *line, int number)
+void run(const tline *line, int number)
 {
     char **arguments;
     char *command;
@@ -305,4 +201,152 @@ void restore(const int stdinfd, const int stdoutfd, const int stderrfd)
     dup2(stderrfd, STDERR_FILENO);
     dup2(stdinfd, STDIN_FILENO);
     dup2(stdoutfd, STDOUT_FILENO);
+}
+
+/**
+ * Executes a series of commands specified in the given command line structure.
+ *
+ * @param line A data structure representing a command line with multiple
+ * commands.
+ *
+ * This function takes a `tline` command line structure as input and executes
+ * the commands sequentially ensuring proper communication between consecutive
+ * commands, managing the flow of input and output through pipes.
+ *
+ * Example:
+ *   tline *line = tokenize("ls -l | grep .txt | wc -l");
+ *   executeCommands(line);
+ *
+ * Note:
+ *   This function relies on the `parser.h` library and auxiliary functions
+ *   like `store`, `redirect`, `run`, `restore`, and assumes the existence of
+ *   constants like `PIPE_READ`, `PIPE_WRITE`, or `FORK_CHILD`.
+ */
+void executeCommands(tline *line)
+{
+    int stdinfd, stdoutfd, stderrfd;
+    int commands, command;
+    int next, even, last;
+    pid_t pid;
+    int p[PIPE], p2[PIPE];
+
+    store(&stdinfd, &stdoutfd, &stderrfd);
+
+    commands = line->ncommands;
+    next = commands > 1;
+
+    if (next)
+    {
+        pipe(p);
+    }
+
+    pid = fork();
+
+    if (pid == FORK_CHILD)
+    {
+        redirect(line);
+
+        if (next)
+        {
+            // Does not receive input from anyone
+            close(p[PIPE_READ]);
+
+            // Pipe `stdout` output for next command input
+            dup2(p[PIPE_WRITE], STDOUT_FILENO);
+            close(p[PIPE_WRITE]);
+        }
+
+        run(line, 0);
+    }
+    else
+    {
+        // Only reads from pipe to provide input for next command
+        close(p[PIPE_WRITE]);
+
+        // Clears `stdout` and `stdin` in case there are following commands
+        restore(stdinfd, stdoutfd, stderrfd);
+
+        wait(NULL);
+
+        for (command = 1; next && command < commands; command++)
+        {
+            even = command % 2 == 0;
+            last = command == commands - 1;
+
+            // Restore the child process writing pipe to prevent errors
+            if (even)
+            {
+                pipe(p);
+            }
+            else
+            {
+                pipe(p2);
+            }
+
+            pid = fork();
+
+            if (pid == FORK_CHILD)
+            {
+                redirect(line);
+
+                // Reads from one pipe and writes to another based on parity
+                if (even)
+                {
+                    dup2(p2[PIPE_READ], STDIN_FILENO);
+                    if (!last)
+                    {
+                        dup2(p[PIPE_WRITE], STDOUT_FILENO);
+                    }
+                }
+                else
+                {
+                    dup2(p[PIPE_READ], STDIN_FILENO);
+                    if (!last)
+                    {
+                        dup2(p2[PIPE_WRITE], STDOUT_FILENO);
+                    }
+                }
+                close(p[PIPE_READ]);
+                close(p[PIPE_WRITE]);
+                close(p2[PIPE_READ]);
+                close(p2[PIPE_WRITE]);
+
+                run(line, command);
+            }
+            else
+            {
+                if (even)
+                {
+                    dup2(STDIN_FILENO, p[PIPE_WRITE]);
+                    close(p[PIPE_WRITE]);
+
+                    // Closes unnecessary pipes for the next odd child
+                    close(p2[PIPE_READ]);
+                    if (last)
+                    {
+                        close(p[PIPE_READ]);
+                        close(p2[PIPE_WRITE]);
+                    }
+                }
+                else
+                {
+                    dup2(STDIN_FILENO, p2[PIPE_WRITE]);
+                    close(p2[PIPE_WRITE]);
+
+                    // Closes unnecessary pipes for the next even child
+                    close(p[PIPE_READ]);
+                    if (last)
+                    {
+                        close(p2[PIPE_READ]);
+                        close(p[PIPE_WRITE]);
+                    }
+                }
+
+                wait(NULL);
+            }
+        }
+
+        // Finish by cleaning `stdout` and `stdin` again for next iteration
+        restore(stdinfd, stdoutfd, stderrfd);
+    }
 }
